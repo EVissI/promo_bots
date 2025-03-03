41ba2164b7fcb67d7662b9168f15112444a5437c
viss <egor-berezin@bk.ru>
2025-03-04 06:52:27 +1000

    commit

--- app/aiogram/main.py
+++ app/aiogram/main.py
@@ -72,7 +72,7 @@ async def check_subscriptions():
             for user_info in users:
                 user_id = user_info.telegram_id
                 subscription_end = user_info.subscription_end
-                if subscription_end is not None:
+                if subscription_end.isinstance(datetime):
                     if subscription_end <= current_date:
                         user_info.promo_code = None
                         user_info.subscription_end = None
